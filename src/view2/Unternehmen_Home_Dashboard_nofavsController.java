11d275a2fceafb5ee462cfd90daad3b0247e2ae5
domin <domin@Surface-Dominik.fritz.box>
2017-10-06 22:22:05 +0200

    Controller

--- src/view2/Unternehmen_Home_Dashboard_nofavsController.java
+++ src/view2/Unternehmen_Home_Dashboard_nofavsController.java
@@ -12,77 +12,77 @@ import javafx.stage.Stage;
 
 public class Unternehmen_Home_Dashboard_nofavsController {
 
-    @FXML
-    private ImageView homebutton;
+	@FXML
+	private ImageView homebutton;
 
-    @FXML
-    private ImageView profilbutton;
+	@FXML
+	private ImageView profilbutton;
 
-    @FXML
-    private ImageView searchbutton;
+	@FXML
+	private ImageView searchbutton;
 
-    @FXML
-    private ImageView settingsbutton;
+	@FXML
+	private ImageView settingsbutton;
 
-    @FXML
-    private ImageView signoutbutton;
+	@FXML
+	private ImageView signoutbutton;
 
-    @FXML
-    private ImageView userpicture;
+	@FXML
+	private ImageView userpicture;
 
-    @FXML
-    private ImageView addofferbutton;
-    
-    void changescene(String fxmlname) throws IOException {
-      	 
-      	 //schliesst aktuelles Fenster
-       	Stage stage2 = (Stage) homebutton.getScene().getWindow();
-              stage2.close();
-       	
-      	 Stage stage = new Stage();
-           stage.setTitle("X2Success");
-           Pane myPane = null;
-           myPane = FXMLLoader.load(getClass().getResource(fxmlname));
-           Scene scene = new Scene(myPane);
-           stage.setScene(scene);
-           stage.show();
+	@FXML
+	private ImageView addofferbutton;
 
-       }
+	void changescene(String fxmlname) throws IOException {
 
-    @FXML
-    void createnewoffer(MouseEvent event) throws IOException {
-    	changescene("Unternehmen_createoffer.fxml");
-    }
+		// schliesst aktuelles Fenster
+		Stage stage2 = (Stage) homebutton.getScene().getWindow();
 
-    @FXML
-    void openHome(MouseEvent event) throws IOException {
+		Stage stage = new Stage();
+		stage.setTitle("X2Success");
+		Pane myPane = null;
+		myPane = FXMLLoader.load(getClass().getResource(fxmlname));
+		Scene scene = new Scene(myPane);
+		stage2.setScene(scene);
 
-    	//Abfrage ob es schon Favoriten gibt
-   if(false)     changescene("Unternehmen_Home_Dashboard_nofavs.fxml");
-   else      changescene("Unternehmen_Home_Dashboard_favs.fxml");
-    
-    }
+	}
 
-    @FXML
-    void openProfil(MouseEvent event) throws IOException {
-    	changescene("Unternehmen_Profil.fxml");
-    }
+	@FXML
+	void createnewoffer(MouseEvent event) throws IOException {
+		changescene("Unternehmen_createoffer.fxml");
+	}
 
-    @FXML
-    void openSearch(MouseEvent event) throws IOException {
-    	changescene("Unternehmen_Suche_Dashboard.fxml");
+	@FXML
+	void openHome(MouseEvent event) throws IOException {
 
-    }
+		// Abfrage ob es schon Favoriten gibt
+		if (false)
+			changescene("Unternehmen_Home_Dashboard_nofavs.fxml");
+		else
+			changescene("Unternehmen_Home_Dashboard_favs.fxml");
 
-    @FXML
-    void openSettings(MouseEvent event) throws IOException {
-    	changescene("Unternehmen_Settings_Dashboard.fxml");
+	}
 
-    }
+	@FXML
+	void openProfil(MouseEvent event) throws IOException {
+		changescene("Unternehmen_Profil.fxml");
+	}
 
-    @FXML
-    void openSignOut(MouseEvent event) throws IOException {
-    	changescene("Einloggen.fxml");
-    }
+	@FXML
+	void openSearch(MouseEvent event) throws IOException {
+		changescene("Unternehmen_Suche_Dashboard.fxml");
+
+	}
+
+	@FXML
+	void openSettings(MouseEvent event) throws IOException {
+		changescene("Unternehmen_Settings_Dashboard.fxml");
+
+	}
+
+	@FXML
+	void openSignOut(MouseEvent event) throws IOException {
+		changescene("Einloggen.fxml");
+	}
 
 }
