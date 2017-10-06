e1330362863e4bbfaf22e6c5cd6384834d980793
domin <domin@141.31.65.99>
2017-10-06 10:26:24 +0200

    Message

--- src/persistence/DBConnection.java
+++ src/persistence/DBConnection.java
@@ -5,35 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	// protected Connection connect = null;
-
-	// public Connection getConnection() throws DAOException {
-	// Connection result = null;
-	// String dbConnString = getConnectionString(aDbName);
-	// try {
-	//
-	// DataSource datasource = (DataSource)initialContext.lookup(dbConnString);
-	// if ( datasource == null ){
-	// fLogger.severe("Datasource is null for : " + dbConnString);
-	// }
-	// result = datasource.getConnection();
-	// }
-	// catch (NamingException ex){
-	// throw new DAOException(
-	// "Config error with JNDI and datasource, for db " + Util.quote(dbConnString),
-	// ex
-	// );
-	// }
-	// catch (SQLException ex ){
-	// throw new DAOException(
-	// "Cannot get JNDI connection from datasource, for db " +
-	// Util.quote(dbConnString),
-	// ex
-	// );
-	// }
-	// return result;
-	//
-	// }
 
 	protected final Connection getConnection() throws ClassNotFoundException, SQLException {
 		Connection connect = null;
