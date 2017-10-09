0169a0d3d3f188c7c0be26211ca173eb0198598c
domin <domin@141.31.65.99>
2017-10-09 12:05:17 +0200

    Bugfixes

--- src/persistence/DBConnection.java
+++ src/persistence/DBConnection.java
@@ -10,9 +10,13 @@ import java.sql.SQLException;
  */
 public class DBConnection {
 
-	protected final Connection getConnection() throws ClassNotFoundException, SQLException {
+	protected final Connection getConnection() throws SQLException {
 		Connection connect = null;
-		Class.forName("com.mysql.jdbc.Driver");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		connect = DriverManager.getConnection("jdbc:mysql://localhost/x2succes?" + "user=root&password=password");
 		// TODO Password in configfile
 		return connect;
